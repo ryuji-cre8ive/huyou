84aa727afc28cd8a266650e2c4be47d485d41f99
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2023-02-09 11:29:03 +0900

    :sparkles: graphqlクライアント用に関数作成

--- lib/graphql.ts
+++ lib/graphql.ts
@@ -1,4 +1,31 @@
 import { createClient } from 'urql'
+import { GraphQLClient } from 'graphql-request'
+import { getSdk } from '~/generated/server'
+
 export const client = createClient({
   url: process.env.GO_GRAPHQL_BASE_URL || 'http://localhost:8080/query',
 })
+
+export const getDataWithQuery = async<T>(query: String, arg?: T, limit?: Number): Promise<any> => {
+  let data
+  const server = new GraphQLClient(process.env.GO_GRAPH_SERVER || 'http://localhost:8080/query')
+  const sdk = getSdk(server)
+  if (query == 'FetchUser') {
+    data = await sdk.FetchUser()
+  }else if (query == 'FindItem') {
+    data = await sdk.FindItem({id: String(arg)})
+  }else if (query == 'ShopItemIDs') {
+    data = await sdk.ShopItemIDs()
+  }else if (query == 'ShopItemTop') {
+    data = await sdk.ShopItemsTop()
+  }else if (query == 'UserIDs') {
+    data = await sdk.UserIDs()
+  } else if (query == 'FindUser') {
+    data = await sdk.FindUser({id: String(arg)})
+  }
+  else {
+    data = await sdk.FetchUser()
+    alert('you are wrong args...')
+  }
+  return data
+}
