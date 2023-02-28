06a1a38f1037635e4b924dda4f95395c4f3380aa
ryuji-cre8ive <ryujisoccer0327@gmail.com>
2023-02-28 11:21:50 +0900

    :sparkles: ユーザーページを編集

--- generated/graphql.ts
+++ generated/graphql.ts
@@ -1,307 +1,244 @@
-import gql from 'graphql-tag'
-import * as Urql from 'urql'
-export type Maybe<T> = T | null
-export type InputMaybe<T> = Maybe<T>
-export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] }
-export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> }
-export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> }
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>
+import gql from 'graphql-tag';
+import * as Urql from 'urql';
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
-  ID: string
-  String: string
-  Boolean: boolean
-  Int: number
-  Float: number
-}
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+};
 
 export type Comment = {
-  __typename?: 'Comment'
-  content: Scalars['String']
-  id: Scalars['ID']
-  shopItemID: Scalars['ID']
-  userID: Scalars['String']
-}
+  __typename?: 'Comment';
+  content: Scalars['String'];
+  id: Scalars['ID'];
+  shopItemID: Scalars['ID'];
+  userID: Scalars['String'];
+};
 
 export type Mutation = {
-  __typename?: 'Mutation'
-  createComment: Comment
-  createShopItem: ShopItem
-  createUser: User
-}
+  __typename?: 'Mutation';
+  createComment: Comment;
+  createShopItem: ShopItem;
+  createUser: User;
+};
+
 
 export type MutationCreateCommentArgs = {
-  input: NewComment
-}
+  input: NewComment;
+};
+
 
 export type MutationCreateShopItemArgs = {
-  input: NewShopItem
-}
+  input: NewShopItem;
+};
+
 
 export type MutationCreateUserArgs = {
-  input: NewUser
-}
+  input: NewUser;
+};
 
 export type NewComment = {
-  content: Scalars['String']
-  id: Scalars['ID']
-  shopItemID: Scalars['ID']
-  userID: Scalars['String']
-}
+  content: Scalars['String'];
+  id: Scalars['ID'];
+  shopItemID: Scalars['ID'];
+  userID: Scalars['String'];
+};
 
 export type NewShopItem = {
-  description?: InputMaybe<Scalars['String']>
-  good?: InputMaybe<Scalars['Int']>
-  id: Scalars['ID']
-  image: Scalars['String']
-  prise: Scalars['Int']
-  title: Scalars['String']
-}
+  description?: InputMaybe<Scalars['String']>;
+  good?: InputMaybe<Scalars['Int']>;
+  id: Scalars['ID'];
+  image: Scalars['String'];
+  prise: Scalars['Int'];
+  title: Scalars['String'];
+};
 
 export type NewUser = {
-  assessment?: InputMaybe<Scalars['Int']>
-  id: Scalars['ID']
-  image?: InputMaybe<Scalars['String']>
-  name: Scalars['String']
-}
+  assessment?: InputMaybe<Scalars['Int']>;
+  id: Scalars['ID'];
+  image?: InputMaybe<Scalars['String']>;
+  name: Scalars['String'];
+};
 
 export type Query = {
-  __typename?: 'Query'
-  comments: Array<Maybe<Comment>>
-  item: ShopItem
-  items: Array<ShopItem>
-  user?: Maybe<User>
-  users: Array<User>
-}
+  __typename?: 'Query';
+  comments: Array<Maybe<Comment>>;
+  item: ShopItem;
+  items: Array<ShopItem>;
+  user?: Maybe<User>;
+  users: Array<User>;
+};
+
 
 export type QueryItemArgs = {
-  id: Scalars['ID']
-}
+  id: Scalars['ID'];
+};
+
 
 export type QueryUserArgs = {
-  id: Scalars['ID']
-}
+  id: Scalars['ID'];
+};
 
 export type ShopItem = {
-  __typename?: 'ShopItem'
-  comments?: Maybe<Array<Maybe<Comment>>>
-  description?: Maybe<Scalars['String']>
-  good: Scalars['Int']
-  id: Scalars['ID']
-  image: Scalars['String']
-  prise: Scalars['Int']
-  title: Scalars['String']
-  user: User
-  userID: Scalars['String']
-}
+  __typename?: 'ShopItem';
+  comments?: Maybe<Array<Maybe<Comment>>>;
+  description?: Maybe<Scalars['String']>;
+  good: Scalars['Int'];
+  id: Scalars['ID'];
+  image: Scalars['String'];
+  prise: Scalars['Int'];
+  title: Scalars['String'];
+  user: User;
+  userID: Scalars['String'];
+};
 
 export type User = {
-  __typename?: 'User'
-  ShopItem?: Maybe<Array<ShopItem>>
-  assessment?: Maybe<Scalars['Int']>
-  id: Scalars['ID']
-  image?: Maybe<Scalars['String']>
-  name: Scalars['String']
-}
+  __typename?: 'User';
+  ShopItem?: Maybe<Array<ShopItem>>;
+  assessment?: Maybe<Scalars['Int']>;
+  id: Scalars['ID'];
+  image?: Maybe<Scalars['String']>;
+  name: Scalars['String'];
+};
 
-export type ItemFragmentFragment = {
-  __typename?: 'ShopItem'
-  id: string
-  title: string
-  description?: string | null
-  image: string
-  good: number
-  prise: number
-}
+export type ItemFragmentFragment = { __typename?: 'ShopItem', id: string, title: string, description?: string | null, image: string, good: number, prise: number };
 
-export type FetchUserQueryVariables = Exact<{ [key: string]: never }>
+export type FetchUserQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type FetchUserQuery = {
-  __typename?: 'Query'
-  users: Array<{ __typename?: 'User'; id: string; name: string }>
-}
 
-export type UserIDsQueryVariables = Exact<{ [key: string]: never }>
+export type FetchUserQuery = { __typename?: 'Query', users: Array<{ __typename?: 'User', id: string, name: string }> };
 
-export type UserIDsQuery = {
-  __typename?: 'Query'
-  users: Array<{ __typename?: 'User'; id: string }>
-}
+export type UserIDsQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type ShopItemsTopQueryVariables = Exact<{ [key: string]: never }>
 
-export type ShopItemsTopQuery = {
-  __typename?: 'Query'
-  items: Array<{ __typename?: 'ShopItem'; id: string; title: string; prise: number; image: string }>
-}
+export type UserIDsQuery = { __typename?: 'Query', users: Array<{ __typename?: 'User', id: string }> };
 
-export type ShopItemIDsQueryVariables = Exact<{ [key: string]: never }>
+export type ShopItemsTopQueryVariables = Exact<{ [key: string]: never; }>;
 
-export type ShopItemIDsQuery = {
-  __typename?: 'Query'
-  items: Array<{ __typename?: 'ShopItem'; id: string }>
-}
+
+export type ShopItemsTopQuery = { __typename?: 'Query', items: Array<{ __typename?: 'ShopItem', id: string, title: string, prise: number, image: string }> };
+
+export type ShopItemIDsQueryVariables = Exact<{ [key: string]: never; }>;
+
+
+export type ShopItemIDsQuery = { __typename?: 'Query', items: Array<{ __typename?: 'ShopItem', id: string }> };
 
 export type FindItemQueryVariables = Exact<{
-  id: Scalars['ID']
-}>
+  id: Scalars['ID'];
+}>;
 
-export type FindItemQuery = {
-  __typename?: 'Query'
-  item: {
-    __typename?: 'ShopItem'
-    id: string
-    title: string
-    description?: string | null
-    image: string
-    good: number
-    prise: number
-    user: { __typename?: 'User'; id: string; name: string; assessment?: number | null }
-    comments?: Array<{ __typename?: 'Comment'; content: string; userID: string } | null> | null
-  }
-}
+
+export type FindItemQuery = { __typename?: 'Query', item: { __typename?: 'ShopItem', id: string, title: string, description?: string | null, image: string, good: number, prise: number, user: { __typename?: 'User', id: string, name: string, assessment?: number | null }, comments?: Array<{ __typename?: 'Comment', content: string, userID: string } | null> | null } };
 
 export type FindUserQueryVariables = Exact<{
-  id: Scalars['ID']
-}>
+  id: Scalars['ID'];
+}>;
 
-export type FindUserQuery = {
-  __typename?: 'Query'
-  user?: {
-    __typename?: 'User'
-    name: string
-    image?: string | null
-    assessment?: number | null
-    ShopItem?: Array<{
-      __typename?: 'ShopItem'
-      id: string
-      title: string
-      image: string
-      prise: number
-    }> | null
-  } | null
-}
+
+export type FindUserQuery = { __typename?: 'Query', user?: { __typename?: 'User', name: string, image?: string | null, assessment?: number | null, ShopItem?: Array<{ __typename?: 'ShopItem', id: string, title: string, image: string, prise: number }> | null } | null };
 
 export const ItemFragmentFragmentDoc = gql`
-  fragment ItemFragment on ShopItem {
-    id
-    title
-    description
-    image
-    good
-    prise
-  }
-`
-export const FetchUserDocument = gql`
-  query FetchUser {
-    users {
-      id
-      name
-    }
-  }
-`
-
-export function useFetchUserQuery(
-  options?: Omit<Urql.UseQueryArgs<FetchUserQueryVariables>, 'query'>,
-) {
-  return Urql.useQuery<FetchUserQuery, FetchUserQueryVariables>({
-    query: FetchUserDocument,
-    ...options,
-  })
+    fragment ItemFragment on ShopItem {
+  id
+  title
+  description
+  image
+  good
+  prise
 }
-export const UserIDsDocument = gql`
-  query UserIDs {
-    users {
-      id
-    }
+    `;
+export const FetchUserDocument = gql`
+    query FetchUser {
+  users {
+    id
+    name
   }
-`
+}
+    `;
+
+export function useFetchUserQuery(options?: Omit<Urql.UseQueryArgs<FetchUserQueryVariables>, 'query'>) {
+  return Urql.useQuery<FetchUserQuery, FetchUserQueryVariables>({ query: FetchUserDocument, ...options });
+};
+export const UserIDsDocument = gql`
+    query UserIDs {
+  users {
+    id
+  }
+}
+    `;
 
 export function useUserIDsQuery(options?: Omit<Urql.UseQueryArgs<UserIDsQueryVariables>, 'query'>) {
-  return Urql.useQuery<UserIDsQuery, UserIDsQueryVariables>({ query: UserIDsDocument, ...options })
-}
+  return Urql.useQuery<UserIDsQuery, UserIDsQueryVariables>({ query: UserIDsDocument, ...options });
+};
 export const ShopItemsTopDocument = gql`
-  query ShopItemsTop {
-    items {
+    query ShopItemsTop {
+  items {
+    id
+    title
+    prise
+    image
+  }
+}
+    `;
+
+export function useShopItemsTopQuery(options?: Omit<Urql.UseQueryArgs<ShopItemsTopQueryVariables>, 'query'>) {
+  return Urql.useQuery<ShopItemsTopQuery, ShopItemsTopQueryVariables>({ query: ShopItemsTopDocument, ...options });
+};
+export const ShopItemIDsDocument = gql`
+    query ShopItemIDs {
+  items {
+    id
+  }
+}
+    `;
+
+export function useShopItemIDsQuery(options?: Omit<Urql.UseQueryArgs<ShopItemIDsQueryVariables>, 'query'>) {
+  return Urql.useQuery<ShopItemIDsQuery, ShopItemIDsQueryVariables>({ query: ShopItemIDsDocument, ...options });
+};
+export const FindItemDocument = gql`
+    query FindItem($id: ID!) {
+  item(id: $id) {
+    ...ItemFragment
+    user {
+      id
+      name
+      assessment
+    }
+    comments {
+      content
+      userID
+    }
+  }
+}
+    ${ItemFragmentFragmentDoc}`;
+
+export function useFindItemQuery(options: Omit<Urql.UseQueryArgs<FindItemQueryVariables>, 'query'>) {
+  return Urql.useQuery<FindItemQuery, FindItemQueryVariables>({ query: FindItemDocument, ...options });
+};
+export const FindUserDocument = gql`
+    query FindUser($id: ID!) {
+  user(id: $id) {
+    name
+    image
+    assessment
+    ShopItem {
       id
       title
+      image
       prise
-      image
     }
   }
-`
-
-export function useShopItemsTopQuery(
-  options?: Omit<Urql.UseQueryArgs<ShopItemsTopQueryVariables>, 'query'>,
-) {
-  return Urql.useQuery<ShopItemsTopQuery, ShopItemsTopQueryVariables>({
-    query: ShopItemsTopDocument,
-    ...options,
-  })
 }
-export const ShopItemIDsDocument = gql`
-  query ShopItemIDs {
-    items {
-      id
-    }
-  }
-`
+    `;
 
-export function useShopItemIDsQuery(
-  options?: Omit<Urql.UseQueryArgs<ShopItemIDsQueryVariables>, 'query'>,
-) {
-  return Urql.useQuery<ShopItemIDsQuery, ShopItemIDsQueryVariables>({
-    query: ShopItemIDsDocument,
-    ...options,
-  })
-}
-export const FindItemDocument = gql`
-  query FindItem($id: ID!) {
-    item(id: $id) {
-      ...ItemFragment
-      user {
-        id
-        name
-        assessment
-      }
-      comments {
-        content
-        userID
-      }
-    }
-  }
-  ${ItemFragmentFragmentDoc}
-`
-
-export function useFindItemQuery(
-  options: Omit<Urql.UseQueryArgs<FindItemQueryVariables>, 'query'>,
-) {
-  return Urql.useQuery<FindItemQuery, FindItemQueryVariables>({
-    query: FindItemDocument,
-    ...options,
-  })
-}
-export const FindUserDocument = gql`
-  query FindUser($id: ID!) {
-    user(id: $id) {
-      name
-      image
-      assessment
-      ShopItem {
-        id
-        title
-        image
-        prise
-      }
-    }
-  }
-`
-
-export function useFindUserQuery(
-  options: Omit<Urql.UseQueryArgs<FindUserQueryVariables>, 'query'>,
-) {
-  return Urql.useQuery<FindUserQuery, FindUserQueryVariables>({
-    query: FindUserDocument,
-    ...options,
-  })
-}
+export function useFindUserQuery(options: Omit<Urql.UseQueryArgs<FindUserQueryVariables>, 'query'>) {
+  return Urql.useQuery<FindUserQuery, FindUserQueryVariables>({ query: FindUserDocument, ...options });
+};
--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -3,7 +3,10 @@ import React from 'react'
 import { getDataWithQuery } from 'lib/graphql'
 import { GraphQLClient } from 'graphql-request'
 import { getSdk, UserIDsQuery, FindUserQuery } from '~/generated/server'
-import { User } from '~/generated/graphql'
+import type { User } from '~/generated/graphql'
+import { Box, Rating, Button } from '@mui/material'
+import NextImage from 'next/image'
+
 export const getStaticPaths: GetStaticPaths = async () => {
   const data: UserIDsQuery = await getDataWithQuery('UserIDs')
   const paths = data.users.map((user) => ({
@@ -38,7 +41,17 @@ interface Params {
 export const UserPage: NextPage<Params> = ({ user }) => {
   return (
     <>
-      <h1>this is {user.name} user page</h1>
+      <Box sx={{display: 'flex'}}>
+        <Box sx={{position: 'relative', width: { xs: '50px', sm: '100px' },
+              height: { xs: '50px', sm: '100px' },}}>
+          <NextImage src={user.image ? user.image : '/vercel.svg'} alt='user image' fill style={{ margin: '0 5%', backgroundColor: '#000', borderRadius: '50%' }}/>
+        </Box>
+        <Box sx={{margin: '0 20px'}}>
+          <p style={{margin: '0'}}>{user.name}</p>
+          <Rating readOnly value={user.assessment} size='small'></Rating>
+        </Box>
+        <Button variant='outlined' color='error' size='small'>フォロー</Button>
+      </Box>
     </>
   )
 }
