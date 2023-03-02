b0c915fb662252d6229228de6a154efc88cae0f3
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2023-03-02 17:25:00 +0900

    :sparkles: prod用サーバー追加

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    Server: isProd ? 'https://server-6fldog5s3a-uc.a.run.app/query' : 'localhost:8080/query'
+    Server: isProd ? 'https://server-6fldog5s3a-uc.a.run.app/query' : 'http://localhost:8080/query'
   }
 }
 
