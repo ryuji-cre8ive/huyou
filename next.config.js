3770f6364bcc36f2aee89d68e72617b408c4e2f0
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2023-03-02 14:59:09 +0900

    :green_heart: connect to cloudrun

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
   reactStrictMode: false,
+  env: {
+    server: isProd ? 'https://server-6fldog5s3a-uc.a.run.app/query' : 'localhost:8080/query'
+  }
 }
 
 module.exports = nextConfig
