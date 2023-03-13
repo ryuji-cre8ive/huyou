73b20f5c543eee06ae022dd064c2a202d6018c7e
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2023-03-13 12:38:10 +0900

    :bug: blocking -> true

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -16,7 +16,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
   }))
   return {
     paths,
-    fallback: 'blocking', // can also be true or 'blocking'
+    fallback: true, // can also be true or 'blocking'
   }
 }
 
