b1af268fab8c55e10070894d659eeb08699e2117
Ryuji Kokubu <z.ryuji.kokubu@gree.net>
2023-04-18 21:42:20 +0900

    :sparkles: エラった時に再帰

--- pages/api/getProfImage.ts
+++ pages/api/getProfImage.ts
@@ -24,6 +24,11 @@ export default async function handler(req: any, res: any) {
     expires: Date.now() + 7 * 60 * 60 * 1000, // 1週間有効
   }
 
-  const [url] = await file.getSignedUrl(options)
-  return res.status(200).json({ url })
+  try {
+    const [url] = await file.getSignedUrl(options)
+    return res.status(200).json({ url })
+  } catch(e) {
+    console.log("error: ",e)
+    handler(req, res)
+  }
 }
